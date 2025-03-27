59d76f4580caff559dd127ee7628922bb06c589e
Jaewon <dhwodnjs0827@naver.com>
2025-03-28 03:07:32 +0900

    [Chore] 작업 폴더 추가 및 StageData SO 추가
    # OJW.meta
    - 개인 작업 폴더 추가
    
    # StageData.cs
    - 스테이지 정보 SO 스크립트 추가

---
+++ LeftOneDead_Team16/Assets/90. WorkSpace/OJW/Scripts/StageData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stage", menuName = "SO/StageData")]
+public class StageData : ScriptableObject
+{
+    public int chapter;
+    public int stage;
+}
