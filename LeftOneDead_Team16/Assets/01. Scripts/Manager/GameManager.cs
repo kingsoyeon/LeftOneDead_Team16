6bc09e67dbf2646d45f765fbd9a51036a5fb770c
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-28 19:01:29 +0900

    [Feat] 씬 전환 관리
    #GameManager.cs
    - 현 게임 상태 enum 정의
    - 로딩씬 호출 함수
    
    #SceneLoader.cs
    - 로딩씬에 존재
    - 씬 전환 진행도 바 & 페이드 효과
    
    # LoadingUI.cs
    - 캔버스 그룹
    - 프리팹화 예정

--- LeftOneDead_Team16/Assets/01. Scripts/Manager/GameManager.cs
+++ LeftOneDead_Team16/Assets/01. Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 프레임 설정
@@ -8,14 +9,58 @@ using UnityEngine;
 /// </summary>
 public class GameManager : Singleton<GameManager>
 {
+    /// <summary>
+    /// 게임 상태
+    /// (로비 / 로딩 / 인게임)
+    /// </summary>
+    public enum GameState 
+    { 
+        Lobby, 
+        Loading, 
+        Setting,
+        InGame,
+    }
+
+    public GameState CurrentState { get; private set; }
+
     void Start()
     {
         Application.targetFrameRate = 60;
+        if(CurrentState == GameState.InGame) Cursor.lockState = CursorLockMode.Locked;
+
+        // ESC를 누르면 CurrentState == Setting으로 전환해야 함. 어디서?
     }
 
-    
-    void Update()
+    /// <summary>
+    /// 1. 로비에서 스테이지 진입 버튼 2. 2번째 스테이지를 시작할 때 [최종 'StageScene'으로 전환]
+    /// </summary>
+    public void StageStart()
     {
-        
+        CurrentState = GameState.Loading;
+        SceneLoader.sceneName = SceneName.StageScene;
+        SceneLoader.nextState = GameState.InGame;
+        SceneManager.LoadScene("LoadingScene");
+
+    }
+
+    /// <summary>
+    /// 1. 2번째 스테이지를 깬 후 2. 게임 중 게임 나가기 버튼 [최종 '로비씬'으로 전환]
+    /// </summary>
+    public void ChangeToLobby()
+    {
+        CurrentState = GameState.Loading;
+        SceneLoader.sceneName = SceneName.MainScene;
+        SceneLoader.nextState = GameState.Lobby;
+        SceneManager.LoadScene("LoadingScene");
+
+    }
+
+    /// <summary>
+    /// 새롭게 전환 될 State를 설정하는 함수
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetGameState(GameState nextState)
+    {
+        CurrentState = nextState;
     }
 }
---
+++ LeftOneDead_Team16/Assets/01. Scripts/UI/LoadingUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class LoadingUI : BaseUI
+{
+    public CanvasGroup canvasGroup;
+    public Image progressBar;
+    public Image stageImage; // 스테이지 이미지
+    //public TextMeshProUGUI tipText; // 랜덤 팁 
+}
