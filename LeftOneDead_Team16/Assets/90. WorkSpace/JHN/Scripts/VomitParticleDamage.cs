ef0629524b80e55e975b06d3970ec8d72a865d49
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-04-01 00:05:03 +0900

    [Feat] vomit 파티클 시스템 추가
    # VomitParticleDamage.cs
    - 파티클에 트리거&콜리전 추가

--- LeftOneDead_Team16/Assets/90. WorkSpace/JHN/Scripts/VomitParticleDamage.cs
+++ LeftOneDead_Team16/Assets/90. WorkSpace/JHN/Scripts/VomitParticleDamage.cs
@@ -1,16 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class VomitParticleDamage : MonoBehaviour
 {
     public int damage = 10;
+    public float poisonDuration = 2.5f;
 
+    Coroutine poisonCoroutine;
+
+    private ParticleSystem ps;
+    private List<ParticleSystem.Particle> enter = new();
+
+    private int count = 0;
+
+    private void Awake() 
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    
+    /// <summary>
+    /// 포이즌 데미지 주기
+    /// </summary>
+    /// <returns></returns> 
+    IEnumerator PoisonCoroutine()
+    {
+        Debug.Log("포이즌 데미지 주기");
+        yield return new WaitForSeconds(poisonDuration);
+        poisonCoroutine = null;
+    }
+
+    /// <summary>
+    /// 파티클 충돌 시 포이즌 데미지 주기
+    /// </summary>
+    /// <param name="other"></param>
     private void OnParticleCollision(GameObject other)
     {
+        if(poisonCoroutine == null)
+        {
+            poisonCoroutine = StartCoroutine(PoisonCoroutine());
+        }
         if(other.TryGetComponent<IDamageable>(out IDamageable damageable))
         {
+            Debug.Log("포이즌 데미지 주기");
             damageable.TakeDamage(damage);
         }
     }
+
+
+    /// <summary>
+    /// 포이즌 트리거
+    /// </summary>
+    void OnParticleTrigger()
+    {
+        Debug.Log("포이즌 트리거");
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);    
+        Debug.Log("numEnter: " + numEnter);
+
+        if(numEnter != 0)
+        {
+            Debug.Log("OnParticleTrigger: 포이즌 데미지 주기");
+            if(poisonCoroutine == null)
+            {
+                poisonCoroutine = StartCoroutine(PoisonCoroutine());
+            }
+        }
+        else
+        {
+            Debug.Log("OnParticleTrigger: 포이즌 데미지 종료");
+        }
+        Debug.Log("count: " + count);
+    }
+
+    // 나는 너무 낡고 지쳤어요...............
+
 }
