40542840c928bd35de688749a4238cc4aa39202f
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-31 16:14:40 +0900

    [Design] Slot UI 디자인 초안
    # WeaponSlot, PistolSlot, ItemSlot
    - 캔버스에 생성
    - 스크립트 생성
    - 로직 구현 필요

--- LeftOneDead_Team16/Assets/01. Scripts/UI/Slot/WeaponSlot.cs
+++ LeftOneDead_Team16/Assets/01. Scripts/UI/Slot/WeaponSlot.cs
@@ -1,19 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class StageScene : MonoBehaviour
+public class WeaponSlot : MonoBehaviour
 {
-    
-    
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-
-
+    // Update is called once per frame
     void Update()
     {
         
