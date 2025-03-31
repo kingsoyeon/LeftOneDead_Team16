8c072a458b86b6525199d1c8b34a1ee8d31dd214
321213132 <minhun5257@gmail.com>
2025-03-31 12:53:04 +0900

    [Feat]플레이어 기능 구현
    이동 및 점프, 달리기 기능

--- LeftOneDead_Team16/Assets/01. Scripts/Interfaces/IState.cs
+++ LeftOneDead_Team16/Assets/01. Scripts/Interfaces/IState.cs
@@ -1,4 +1,36 @@
+using System.Diagnostics;
+
 public interface IState
 {
+    public void Enter();
+    public void Exit();
+    public void HandleInput();
+    public void Uqdate();
+    public void PhysicsUpdate();
+}
+public abstract class StateMachine
+{
+    protected IState currentState;
 
+    public void ChageState(IState state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState?.Enter();
+    }
+
+    public void HandleInput()
+    {
+        currentState?.HandleInput();
+    }
+
+    public void Uqdate()
+    {
+        currentState?.Uqdate();
+    }
+
+    public void PhysicsUpdate()
+    {
+        currentState?.PhysicsUpdate();
+    }
 }
