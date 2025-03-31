d21b2e26ab951ef93797876f04ef9ea8777627fb
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-31 14:33:46 +0900

    [Design] 로딩 UI 디자인
    #LoadingUI.cs
    - 필요한 컴포넌트 추가
    - 추후 맵 이미지 캡쳐해서 넣을 예정
    - 스테이지 데이터와 스테이지 이름 연결 필요

--- LeftOneDead_Team16/Assets/01. Scripts/UI/LoadingUI.cs
+++ LeftOneDead_Team16/Assets/01. Scripts/UI/LoadingUI.cs
@@ -10,4 +10,7 @@ public class LoadingUI : ScreenUI
     public Image progressBar;
     public Image stageImage; // 스테이지 이미지
     //public TextMeshProUGUI tipText; // 랜덤 팁 
+    public TextMeshProUGUI stageNameText;
+    //public TextMeshProUGUI stageInfoText; 스테이지 정보
+    // public 
 }
