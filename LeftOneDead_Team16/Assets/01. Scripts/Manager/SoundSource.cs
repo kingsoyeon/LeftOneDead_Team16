dc9ae8bb2c47ad50ea571e817bdb66648c2b8936
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-04-02 14:20:29 +0900

    [Feat] SoundManager 생성
    # SoundManager.cs
    - 씬 별로 설정된 BGM 재생
    - SoundSource ->  prefab 필요 추가예정
    - 볼륨 저장 기능 -> settingUI와 연결 필요 추가예정
    
    # SoundSource.cs
    - Play(clip, sfxVolume, sfxPitchVariance)
    - 프리팹에 붙이기

---
+++ LeftOneDead_Team16/Assets/01. Scripts/Manager/SoundSource.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSource : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public void Play(AudioClip clip, float sfxVolume, float sfxPitchVariance)
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+
+        CancelInvoke();
+
+        audioSource.clip = clip;
+        audioSource.volume = sfxVolume;
+        audioSource.Play();
+        audioSource.pitch = 1f + Random.Range(-sfxPitchVariance, sfxPitchVariance);
+
+        Invoke("Disable", clip.length + 2);
+    }
+    public void Disable()
+    {
+        audioSource.Stop();
+        Destroy(this.gameObject);
+    }
+}
